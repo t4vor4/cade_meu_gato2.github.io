af69c4debece041e4bf6e7ed58b9e122c035cd61
t4vor4 <ftavora.santos@gmail.com>
2019-04-02 12:44:28 -0300

    estou transformando a casa do jogo em uma cidade, assim, o que eram comodos agora sao locais da cidade. Vou ter que adaptar a historia para se encaixar com o novo layout

--- src/locais.js
+++ src/locais.js
@@ -1,50 +1,127 @@
+// const estes_locais_old = [
+//     {
+//         id: 'sala',
+//         nome: 'Sala de Estar',
+//         artigo: ['a','na'],
+//         descricao: desc.sala,
+//         ocupantes: [],
+//         dicas: dicas2.sala
+//     }, 
+//     {
+//         id: 'cozinha',
+//         nome: 'Cozinha',
+//         artigo: ['a','na'],
+//         descricao: desc.cozinha,
+//         ocupantes: [],
+//         dicas: dicas2.cozinha
+//     },
+//     {
+//         id: 'banheiro',
+//         nome: 'Banheiro',
+//         artigo: ['o','no'],
+//         descricao: desc.banheiro,
+//         ocupantes: [],
+//         dicas: dicas2.banheiro
+//     },
+//     {
+//         id: 'quarto',
+//         nome: 'Quarto',
+//         artigo: ['o','no'],
+//         descricao: desc.quarto,
+//         ocupantes: [],
+//         dicas: dicas2.quarto
+//     },
+//     {
+//         id: 'lavanderia',
+//         nome: 'Lavanderia',
+//         artigo: ['a','na'],
+//         descricao: desc.lavanderia,
+//         ocupantes: [],
+//         dicas: dicas2.lavanderia
+//     },
+//     {
+//         id: 'varanda',
+//         nome: 'Varanda',
+//         artigo: ['a','na'],
+//         descricao: desc.varanda,
+//         ocupantes: [],
+//         dicas: dicas2.varanda
+//     },
+// ]
+
 const estes_locais = [
     {
-        id: 'sala',
-        nome: 'Sala de Estar',
+        id: 'prefeitura',
+        nome: 'Prefeitura',
         artigo: ['a','na'],
-        descricao: desc.sala,
+        descricao: 'Prefeitura da cidade',
         ocupantes: [],
-        dicas: dicas2.sala
+        dicas: ['Dica facil, Prefeitura','Dica media, Prefeitura','Dica dificil, Prefeitura']
     }, 
     {
-        id: 'cozinha',
-        nome: 'Cozinha',
-        artigo: ['a','na'],
-        descricao: desc.cozinha,
-        ocupantes: [],
-        dicas: dicas2.cozinha
-    },
-    {
-        id: 'banheiro',
-        nome: 'Banheiro',
+        id: 'mercado',
+        nome: 'Mercado',
         artigo: ['o','no'],
-        descricao: desc.banheiro,
+        descricao: 'Mercado da cidade',
         ocupantes: [],
-        dicas: dicas2.banheiro
+        dicas: ['Dica facil, Mercado','Dica media, Mercado','Dica dificil, Mercado']
     },
     {
-        id: 'quarto',
-        nome: 'Quarto',
+        id: 'escola',
+        nome: 'Escola da Cidade',
+        artigo: ['a','na'],
+        descricao: 'Escola da cidade onde estudam crianças e adolescentes',
+        ocupantes: [],
+        dicas: ['Dica facil, Escola','Dica media, Escola','Dica dificil, Escola']
+    },
+    {
+        id: 'floresta',
+        nome: 'Floresta',
+        artigo: ['a','na'],
+        descricao: 'Floresta nos limites da cidade onde vive a fauna local',
+        ocupantes: [],
+        dicas: ['Dica facil, floresta','Dica media, floresta','Dica dificil, floresta']
+    },
+    {
+        id: 'clube',
+        nome: 'Clube',
         artigo: ['o','no'],
-        descricao: desc.quarto,
+        descricao: 'Clube onde as pessoas da cidade se divertem',
         ocupantes: [],
-        dicas: dicas2.quarto
+        dicas: ['Dica facil, clube','Dica media, clube','Dica dificil, clube']
     },
     {
-        id: 'lavanderia',
-        nome: 'Lavanderia',
+        id: 'praca',
+        nome: 'Praça',
         artigo: ['a','na'],
-        descricao: desc.lavanderia,
+        descricao: 'Praça no centro da cidade com coreto',
         ocupantes: [],
-        dicas: dicas2.lavanderia
+        dicas: ['Dica facil, praca','Dica media, praca','Dica dificil, praca']
     },
     {
-        id: 'varanda',
-        nome: 'Varanda',
-        artigo: ['a','na'],
-        descricao: desc.varanda,
+        id: 'acougue',
+        nome: 'Açougue',
+        artigo: ['o','no'],
+        descricao: 'Casa de carnes da cidade',
         ocupantes: [],
-        dicas: dicas2.varanda
+        dicas: ['Dica facil, açougue','Dica media, açougue','Dica dificil, açougue']
     },
+    {
+        id: 'cemiterio',
+        nome: 'Cemitério',
+        artigo: ['o','no'],
+        descricao: 'Ceitério da cidade.',
+        ocupantes: [],
+        dicas: ['Dica facil, Cemiterio','Dica media, Cemitério','Dica dificil, Cemitério']
+    },
+    {
+        id: 'igreja',
+        nome: 'Igreja',
+        artigo: ['a','na'],
+        descricao: 'Igreja da cidade.',
+        ocupantes: [],
+        dicas: ['Dica facil, Igreja','Dica media, Igreja','Dica dificil, Igreja']
+    },
+
 ]
+
